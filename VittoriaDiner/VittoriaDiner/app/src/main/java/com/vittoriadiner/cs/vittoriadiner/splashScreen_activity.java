c836db4d4cdd4135be6e227ed7da18a4d4836ae1
CaityCait <caitlinsistoso@gmail.com>
2018-10-25 22:47:23 -0800

    Initial Commit for Phase 0

---
+++ VittoriaDiner/VittoriaDiner/app/src/main/java/com/vittoriadiner/cs/vittoriadiner/splashScreen_activity.java
@@ -0,0 +1,13 @@
+package com.vittoriadiner.cs.vittoriadiner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splashScreen_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_activity);
+    }
+}
